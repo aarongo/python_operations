96195a86592e881befa391bf2cc7a226dae44567
aarongo <aaron@126.com>
2015-08-18 23:08:07 +0800

    Python----->youxi chugao

--- day4/people.py
+++ day4/people.py
@@ -1,15 +1,24 @@
 # _*_coding:utf-8_*_
 # 定义游戏角色共同属性
-class People(object):
-    # 功能（features）、特征（feature）、工作（job）、年龄（age）、人种（race）、国籍（nationality）、性别（sex）、特长(speciality)、name(姓名)
-    def __init__(self, name, age, sex, job, race, feature, features, speciality, nationality
-                 ):
+# class People(object):
+#     # 功能（features）、特征（feature）、工作（job）、年龄（age）、人种（race）、国籍（nationality）、性别（sex）、特长(speciality)、name(姓名)
+#     def __init__(self, name, age, sex, job, race, feature, features, speciality, nationality
+#                  ):
+#         self.name = name
+#         self.age = age
+#         self.sex = sex
+#         self.job = job
+#         self.race = race
+#         self.feature = feature
+#         self.features = features
+#         self.speciality = speciality
+#         self.nationality = nationality
+
+class Proson(object):
+    def __init__(self, name, age, sex):
         self.name = name
         self.age = age
         self.sex = sex
-        self.job = job
-        self.race = race
-        self.feature = feature
-        self.features = features
-        self.speciality = speciality
-        self.nationality = nationality
+
+
+
