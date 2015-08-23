44cf8abe9a624f1ee7ac19ddeb558bb26ade308d
aarongo <aaron@126.com>
2015-08-23 22:27:20 +0800

    sourceTree git test

--- day4/role.py
+++ day4/role.py
@@ -117,7 +117,7 @@ class Chose(object):
                         print "--------Welcome Login Game--------"
                         break
                     else:
-                        print "\033[31m请选择进行游戏的角色!!!!!!!!!! \033[0m"
+                        print "\033[31m请选择进行游戏的角色 \033[0m"
             except IndexError, err:
                 print err
 run = Chose()
