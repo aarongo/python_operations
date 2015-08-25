61e901184dc5897ea5cf620bcc95c5ab7d73c3d5
aarongo <aaron.docker@gmail.com>
2015-08-25 10:24:24 +0800

    FtpServer First DayHomeworks

--- day5/account.py
+++ day5/account.py
@@ -1,17 +1,11 @@
 # _*_coding:utf-8_*_
 __author__ = 'Lonny.Liu'
 # 定义存储用户
-user1_password = {
+user_password = {
     'test1': '111111',
     'test2': '222222',
-    'test3': '333333'
-}
-user2_password = {
+    'test3': '333333',
     'user1': '123456',
     'user2': 'ftptest',
     'user3': 'test123456'
 }
-
-
-# ******
-# aaaaaa
--- day5/login.py
+++ day5/login.py
@@ -1,16 +1,17 @@
 # _*_coding:utf-8_*_
 __author__ = 'Lonny.Liu'
 import account
-import getpass
-import sys
+
+
 class Login(object):
     def login(self):
         name = raw_input("Please Input Your Name:")
-        if name in account.user1_password:
-            password =getpass.win_getpass("Please Input Your Password:")
-            if password == account.user1_password[name]:
+        if name in account.user_password.keys():
+            password = raw_input("Please Input Password:")
+            if password == account.user_password[name]:
                 print "--------Welcome Login ftpserver-------"
-        elif name in account.user2_password:
-            print "----user2--->",account.user2_password[name]
-run = Login()
-run.login()
+        else:
+            print "--------User does not exist------"
+
+if __name__ == "__main__":
+    Login().login()
